25f3f9f316a23ad5e10ef0813251977520de65c4
muhammmad ismail <mi272401@gmail.com>
2019-09-27 21:36:47 +0700

    add icon and content footer

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -1,15 +1,27 @@
 <template>
-  <footer id="footer" class="container-fluid">
+  <footer id="footer">
     <div class="row">
-      <div class="media-social col-4">
+      <div class="media-social col-md-5">
         <div class="container">
-          We love new friends!
+          <div class="text-center mt-2 mb-3 fs-24">We love new friends!</div>
+          <!-- icons -->
+          <div class="d-flex justify-content-around">
+            <div>
+              <span class="fa-facebook-circle fs-18"></span>
+            </div>
+            <div>
+              <span class="fa-instagram-cirlce fs-18"></span>
+            </div>
+            <div>
+              <span class="fa-twitter-circle fs-18"></span>
+            </div>
+            <div>
+              <span class="fa-facebook-circle fs-18"></span>
+            </div>
+          </div>
         </div>
-        <span class="icon icon-facebook2"></span>
       </div>
-      <div class="copyright col-8 pt-5 pb-5 text-right">
-        @{{nowYear}} Blank. All Rights Reserved.
-      </div>
+      <div class="copyright col-md-7 align-self-center text-right pr-4">@{{nowYear}} Blank. All Rights Reserved.</div>
     </div>
   </footer>
 </template>
@@ -33,11 +45,14 @@ export default {
 
   .media-social {
     background-color: #2980b9;
+    padding: 1.1rem 2rem 1.2rem 5.5rem;
   }
-
+  
   .copyright {
     background-color: transparent;
     font-size: 0.75rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
   }
 }
 </style>
